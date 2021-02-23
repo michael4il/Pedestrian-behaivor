02962558dbbc9e50e0b4930f730338ac282cab9a
michael4il <33546573+michael4il@users.noreply.github.com>
2021-02-24 00:12:10 +0200

    walking added

--- action_estimator.py
+++ action_estimator.py
@@ -15,17 +15,10 @@ from pathlib import Path
 
 def check_phone_left(pose,canvas_3d):
     #If distance between wrist and ear smaller than distance between should and ear 
-    # AND wrist is above shoulder
-    # ear_to_shoulder_distance can be replaced by a certain heurisitic.
-    #ear_to_shoulder_distance = math.sqrt(pow(pose[R_SHO][0]-pose[R_EAR][0],2)+pow(pose[R_SHO][1]-pose[R_EAR][1],2)+pow(pose[R_SHO][2]-pose[R_EAR][2],2))
-    # ear_to_wrist_distance = math.sqrt(pow(pose[R_WRI][0]-pose[R_EAR][0],2)+pow(pose[R_WRI][1]-pose[R_EAR][1],2)+pow(pose[R_WRI][2]-pose[R_EAR][2],2))
-    # if 50>ear_to_wrist_distance and pose[R_WRI][2]>pose[R_SHO][2]:
-        # print("talking on the phone")
-    #l_ear_to_shoulder_distance = math.sqrt(pow(pose[L_SHO][0]-pose[L_EAR][0],2)+pow(pose[L_SHO][1]-pose[L_EAR][1],2)+pow(pose[L_SHO][2]-pose[L_EAR][2],2))
     l_ear_to_wrist_distance = math.sqrt(pow(pose[L_WRI][0]-pose[L_EAR][0],2)+pow(pose[L_WRI][1]-pose[L_EAR][1],2)+pow(pose[L_WRI][2]-pose[L_EAR][2],2))
     #print(l_ear_to_wrist_distance)
     if  50 > l_ear_to_wrist_distance and pose[L_WRI][2]>pose[L_SHO][2]:
-        cv2.putText(canvas_3d, "A terrible leftie is talking on the phone" ,
+        cv2.putText(canvas_3d, "Talking on phone" ,
             (40, 80), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255))
         # print("A leftie is talking on the phone")
     #print(pose)
@@ -36,7 +29,7 @@ def check_lying_down(pose, canvas_3d):
     # distance of ankles to head in z axis is low 
     ankle_median_pos = avg_2_pose(pose[L_ANK], pose[R_ANK])
     
-    print(pose[NECK][2] - ankle_median_pos[2])
+    # print(pose[NECK][2] - ankle_median_pos[2])
     if pose[NECK][2] - ankle_median_pos[2] < 60 and pose[NECK][2] - ankle_median_pos[2] != 0 :
          cv2.putText(canvas_3d, "Lying down" ,
             (40, 80), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255))
@@ -45,21 +38,34 @@ def check_standing(pose, canvas_3d):
     # distance of ankles to head in z axis is high 
     ankle_median_pos = avg_2_pose(pose[L_ANK], pose[R_ANK])
     
-    print(pose[NECK][2] - ankle_median_pos[2])
+    # print(pose[NECK][2] - ankle_median_pos[2])
     if pose[NECK][2] - ankle_median_pos[2] > 100 and pose[NECK][2] - ankle_median_pos[2] != 0 :
-         cv2.putText(canvas_3d, "Standing" ,
-            (40, 120), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255))
+         return True; 
 
 def check_waving_right(pose, canvas_3d):
-    # print(pose[R_WRI][2] - pose[NECK][2] ) 
-    if pose[R_WRI][2] - pose[NECK][2]:
+    # print("right wrist z " , pose[R_WRI][2], "  ||  neck z ",pose[NECK][2])
+    # print("offset   ",pose[R_WRI][2] - pose[NECK][2] ) 
+    if pose[R_WRI][2] - pose[NECK][2]  > 20 and pose[R_WRI][2] != 0 and pose[NECK][2] != 0 and pose[R_WRI][2] != pose[NECK][2] :
         cv2.putText(canvas_3d, "Wave right hand",
         (40, 80), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255))
-        
+def center_mass(pose):
+    x = pose[L_SHO][0] + pose[L_HIP][0] + pose[R_SHO][0] + pose[R_HIP][0]
+    y = pose[L_SHO][1] + pose[L_HIP][1] + pose[R_SHO][1] + pose[R_HIP][1]
+    z = pose[L_SHO][2] + pose[L_HIP][2] + pose[R_SHO][2] + pose[R_HIP][2]
+    return [x / 4, y / 4, z / 4]
     
+def check_moving(pose, canvas_3d, moving_list, idx, last_pose):
+    center_curr = center_mass(pose)
+    center_last = center_mass(last_pose)
+    delta = abs(center_curr[0] - center_last[0]) + abs(center_curr[1] - center_curr[1])
+    if delta < 100:
+        moving_list[idx] = delta
+    print(sum(moving_list[idx - 20: idx]))
+    if sum(moving_list[idx - 20:idx]) > 20:
+        return True
+    return False
+    # look at hip(center mass), if is moving more the delta in X last frames, declare moving 
     
-    
-
 
 kpt_names = ['neck', 'nose',
                 'l_sho', 'l_elb', 'l_wri', 'l_hip', 'l_knee', 'l_ank',
@@ -90,7 +96,7 @@ L_EAR = 18
 
 
 
-fileName = 'waving_4'
+fileName = 'sample3'
 Videofile = Path(fileName).with_suffix('.mp4')
 posesFile = Path(fileName).with_suffix('.npy')
 
@@ -119,19 +125,22 @@ if __name__ == '__main__':
     space_code = 32
     mean_time = 0
     limitter = 0
+    moving_list = [0] * len(all_poses) * 2
+    idx = 30
+    last_pose = all_poses[0][0]  
     for frame in all_poses:
         current_time = cv2.getTickCount()
         if frame is None:
             break
-  
+            
         poses_3d = frame
         edges = []
         
-
+        
         edges = (Plotter3d.SKELETON_EDGES + 19 * np.arange(poses_3d.shape[0]).reshape((-1, 1, 1))).reshape((-1, 2))
         plotter.plot(canvas_3d, poses_3d, edges)
 
-
+        idx = idx + 1 
         for pose in poses_3d:
             #
             # 
@@ -141,13 +150,23 @@ if __name__ == '__main__':
 
             check_phone_left(pose, canvas_3d)
             
-            check_standing(pose,canvas_3d)
                         
             check_waving_right(pose,canvas_3d)
+            
+            standing_flag = check_standing(pose,canvas_3d)
+
+            moving_flag = check_moving(pose, canvas_3d, moving_list, idx ,last_pose)
+            
+            if moving_flag:
+                cv2.putText(canvas_3d, "Walking",
+                (40, 120), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255))
+            else:
+                if standing_flag:
+                    cv2.putText(canvas_3d, "Standing" ,
+                    (40, 120), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255))
         
         
-        
-       
+        last_pose = pose 
 
         #draw_poses(frame, poses_2d)
         current_time = (cv2.getTickCount() - current_time) / cv2.getTickFrequency()
